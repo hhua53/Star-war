d9c34520361fc1ce56d5c01356b947657b9ae495
hhua53 <hhxqweasd@gmail.com>
2020-10-16 20:15:02 -0500

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_014_204_218) do
+ActiveRecord::Schema.define(version: 2020_10_14_204218) do
+
   create_table "film_people", force: :cascade do |t|
     t.integer "film_id", null: false
     t.integer "people_id", null: false
