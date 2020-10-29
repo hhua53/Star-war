6afc28bffda819a4357d955df1c41d501ede7ead
hhua53 <hhxqweasd@gmail.com>
2020-10-29 02:26:40 -0500

    create about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'about/index'
   resources :films
   resources :people
   resources :starships
   resources :planets
+  resources :about
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
