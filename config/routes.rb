044da29739603884949d12e3bea47c18c419595a
hhua53 <hhxqweasd@gmail.com>
2020-10-30 02:34:46 -0500

    edit documentation in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,4 @@ Rails.application.routes.draw do
   resources :planets
   resources :about
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
